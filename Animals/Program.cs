f258fb7dc35bd57e3b4ebc735b4d5f7873a7b15c
T0mikCZ <oplpet95@gmail.com>
2020-06-16 10:57:39 +0200

    New Menu for Workers

--- Animals/Program.cs
+++ Animals/Program.cs
@@ -10,7 +10,7 @@ namespace Animals
     {
         static void Main(string[] args)
         {
-            ZOO  zoo = new ZOO();
+            ZOO zoo = new ZOO();
             Zamestnanci zamestnanci = new Zamestnanci();
 
             string moznost = "";
@@ -43,9 +43,49 @@ namespace Animals
                         zoo.VypisZvirat();
                         Console.ReadKey();
                         break;
-                  //Case 5 je pro ukonceni  
+                    //Case 5 je pro ukonceni  
                     case "6":
+                        Console.Clear();
+                        do
+                        {
 
+                            Console.WriteLine("\nZAMESTNANCI MENU");
+                            Console.WriteLine("1. Pridat Zvire");
+                            Console.WriteLine("2. Odstranit Zvire");
+                            Console.WriteLine("3. Upravit Zvire");
+                            Console.WriteLine("4. Vypis Zvirat");
+                            Console.WriteLine("5. Ukoncit Program");
+                            Console.WriteLine("6. Prepnout na Zamestnantsky mod");
+                            Console.Write("\nVyberte si moznost: ");
+                            zMoznost = Console.ReadLine();
+
+                            switch (moznost)
+                            {
+                                case "1":
+                                    zoo.PridatZvire();
+                                    break;
+                                case "2":
+                                    zoo.OdstranitZvire();
+                                    break;
+                                case "3":
+                                    zoo.UpravitZvire();
+                                    break;
+                                case "4":
+                                    zoo.VypisZvirat();
+                                    Console.ReadKey();
+                                    break;
+                                //Case 5 je pro ukonceni  
+                                case "6":
+
+                                    Console.ReadKey();
+                                    break;
+                                default:
+                                    Console.WriteLine("Spatne zadana moznost");
+                                    break;
+                            }
+
+                            Console.Clear();
+                        } while (moznost != "5");
                         Console.ReadKey();
                         break;
                     default:
